a157c966730e4038383d8b8acce6e8f099dde703
Mumo Carlos <mumo.crls@gmail.com>
2013-09-05 10:49:22 -0500

    refactor subscription dialog properties

--- app/assets/javascripts/store/spree_subscribe.js
+++ app/assets/javascripts/store/spree_subscribe.js
@@ -1,48 +1,55 @@
 $(document).ready(function() {
 
-      $('.subscription-form').dialog({
-        autoOpen: false,
-        height: 250,
-        width: 250,
-        modal: true,
-        buttons: {
-          'Crear': function(){
-            $.post('/client_subscription_intervals', {
-              subscription_interval: {
-              name: $('#subscription_interval_times').val() + ' ' + $('#subscription_interval_time_unit option:selected').text(),
-              times: $('#subscription_interval_times').val(),
-              time_unit: $('#subscription_interval_time_unit option:selected').val(),
-              product_id: $('#product_id').val()
-              }
-            }).done(function(data) {
-              addToDropdown(data);
-            });
-            $( this ).dialog('close');
-          },
-          Cancelar: function() {
-            $( this ).dialog('close');
-          }
-        },
-        close: function() {
-          $('#subscription_interval_times').val('');
-        }
-    });
+  $('.subscription-form').dialog({
+    autoOpen: false,
+    height: 250,
+    width: 250,
+    modal: true,
+    buttons: {
+      Crear: function(){
+        new_interval = { subscription_interval: {} };
+        times = $('#subscription_interval_times');
+        unit = $('#subscription_interval_time_unit');
+
+        new_interval.subscription_interval = {
+          name: times.val() + ' ' + unit.text(),
+          times: times.val(),
+          time_unit: unit.val(),
+          product_id: $('#product_id').val()
+        };
+
+        $.post('/client_subscription_intervals', new_interval)
+          .done(function(data) {
+            addToDropdown(data);
+          });
+        $(this).dialog('close');
+      },
+      Cancelar: function() {
+        $(this).dialog('close');
+      }
+    },
+
+    close: function() {
+      $('#subscription_interval_times').val('');
+    }
+  });
 
   var addToDropdown = function(data){
-    $('#subscriptions_interval_id').prepend($('<option>',{
+    $('#subscriptions_interval_id').prepend($('<option>', {
       value: data.id,
       text: data.name
     }));
     $('#subscriptions_active_1').prop('checked', true);
-    $('#subscriptions_interval_id').attr("disabled", false);
-    $('#subscriptions_interval_id > option[value=' + data.id + ' ]').prop('selected', true);
+    $('#subscriptions_interval_id').attr('disabled', false);
+    $('#subscriptions_interval_id > option[value=' + data.id + ' ]')
+      .prop('selected', true);
     $('#add-to-cart-button').attr('disabled', false);
   }
 
   $(':radio').click(function(e) {
     var oneTime = (e.currentTarget.value == 0);
 
-    $('#subscriptions_interval_id').attr("disabled", oneTime);
+    $('#subscriptions_interval_id').attr('disabled', oneTime);
     var popup = ($('#subscriptions_interval_id option').length == 1 && !oneTime);
     showPopup(popup);
   });
