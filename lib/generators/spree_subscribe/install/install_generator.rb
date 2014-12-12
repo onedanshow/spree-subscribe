8fc18bb8e3e0c65a8853da4e9a72283e731f3f80
Fatty <fattymiller@gmail.com>
2014-12-12 15:30:35 +1000

    asset files moved to backend and frontend instead of old admin and store paths - installer writes these to the vendor directory

--- lib/generators/spree_subscribe/install/install_generator.rb
+++ lib/generators/spree_subscribe/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeSubscribe
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_subscribe\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_subscribe\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require frontend/spree_subscribe\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require backend/spree_subscribe\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_subscribe\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_subscribe\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require frontend/spree_subscribe\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require backend/spree_subscribe\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
